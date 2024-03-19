2fd3582f864eb570f447742217b21e7830a26450
Usuario <Usuario@CPD03-M01>
2024-03-19 11:02:43 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escolha a operação: (+, -, * ou /)");
+            string op = Console.ReadLine();
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine("O resultado da operação é: "+(num1+num2));
+                    break;
+                case "-":
+                    Console.WriteLine("O resultado da operação é: "+(num1-num2));
+                    break;
+                case "*":
+                    Console.WriteLine("O resultado da operação é: "+(num1*num2));
+                    break;
+                case "/":
+                    Console.WriteLine("O resultado da operação é: "+(num1/num2));
+                    break;
+                default: Console.WriteLine("Opção inválida.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
